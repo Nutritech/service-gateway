35ed7f6ef3a91f75db0aa5b2b73e688e700de042
Fernando <fernandochahua1@gmail.com>
2019-11-22 15:17:05 -0500

    primer commit

---
+++ src/test/java/com/nutritech/nu34life/ServiceGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nutritech.nu34life;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
